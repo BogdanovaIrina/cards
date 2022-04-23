c0fc962d961cf16dcc055aa8b7c1c5e78b2740bf
BogdanovaIrina <irina.bogdanova.1990@gmail.com>
2022-04-23 18:15:56 +0300

    4

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => {
     <div className="App">
       <Routes>
         <Route path="/cards" element={<Navigate to="/test" />}/>
-        <Route path={"/login"} element={<Login />} />
+        <Route path={"/cards/login"} element={<Login />} />
         <Route path={"/registr"} element={<Registr />} />
         <Route path={"/profile"} element={<Profile />} />
         <Route path={"/404"} element={<Error />} />
